d7e40fe94644ed3cdd94e18a771fad45cdc0da55
abdulahaddf <abdulahad.df1@gmail.com>
2023-07-22 03:05:04 +0600

    flowbite configured

---
+++ src/Layout/HomeLayout.jsx
@@ -0,0 +1,12 @@
+import Nav from "../Components/Shared/Nav/Nav";
+
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <Nav></Nav>
+        </div>
+    );
+};
+
+export default HomeLayout;
