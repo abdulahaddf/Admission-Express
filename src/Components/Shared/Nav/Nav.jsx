e7e8fa9ec5424e52237a38d94b3ee4213f0ce5b7
abdulahaddf <abdulahad.df1@gmail.com>
2023-07-22 11:19:47 +0600

    nav links

--- src/Components/Shared/Nav/Nav.jsx
+++ src/Components/Shared/Nav/Nav.jsx
@@ -7,24 +7,22 @@ const Nav = () => {
       fluid
       rounded
     >
-      <Navbar.Brand href="https://flowbite-react.com">
-        {/* <img
-          alt="Flowbite React Logo"
-          className="mr-3 h-6 sm:h-9"
-          src="/favicon.svg"
-        /> */}
+      <Link to="/">
+       
         <span className="self-center text-center whitespace-nowrap text-xl font-semibold dark:text-white">
           Admission Express
         </span>
-      </Navbar.Brand>
+      </Link>
       <div className="flex md:order-2">
         <Link className='btn' to="/login">
         Login
         </Link>
         <Navbar.Toggle />
       </div>
-      <Navbar.Collapse>
-        <Link
+      
+      <Navbar.Collapse >
+        <Link className="text-lg hover:text-green hover:scale-125 duration-300"
+        
           active
           to="/"
         >
@@ -32,13 +30,13 @@ const Nav = () => {
             Home
     
         </Link>
-        <Link href="#">
+        <Link className="text-lg hover:text-green hover:scale-125 duration-300" to="/colleges">
           Colleges
         </Link>
-        <Link href="#">
+        <Link className="text-lg hover:text-green hover:scale-125 duration-300" to="/admission">
           Admission
         </Link>
-        <Link href="#">
+        <Link className="text-lg hover:text-green hover:scale-125 duration-300" to="/my-college">
          My College
         </Link>
         
